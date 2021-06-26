1f0619bc12ab2b1b605824d3707d859bec2edee8
Hongbaekson <hbson1@myseneca.ca>
2021-06-26 12:44:44 +0900

    add module

---
+++ scripts/3-13.moudule.js
@@ -0,0 +1,17 @@
+export function log(message) {
+  console.log(message);
+}
+
+export function error(message) {
+  console.error(message);
+}
+
+export const PI = 3.14;
+
+function log(message) {
+  console.log(message);
+}
+
+function error(message) {
+  console.error(message);
+}
---
+++ scripts/3-13.ref.js
@@ -0,0 +1,3 @@
+import { log, error, PI } from "scripts/3-13.moudule.js";
+
+log("REF에서 출력하는 메시지 ")
